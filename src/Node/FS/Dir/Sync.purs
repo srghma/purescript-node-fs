4732d1b1ec0573013dede6188c405359eab98e87
Serhii Khoma <srghma@gmail.com>
2024-10-13 12:50:47 +0700

    refactor: Dir.Sync -> wrong names

--- src/Node/FS/Dir/Sync.purs
+++ src/Node/FS/Dir/Sync.purs
@@ -13,9 +13,9 @@ foreign import closeSyncImpl :: EffectFn1 Dir Unit
 foreign import readSyncImpl :: EffectFn1 Dir (Nullable (Dirent DirentNameString))
 
 -- | Synchronously close the directory's underlying resource handle.
-closeSync :: Dir -> Effect Unit
-closeSync = runEffectFn1 closeSyncImpl
+close :: Dir -> Effect Unit
+close = runEffectFn1 closeSyncImpl
 
 -- | Synchronously read the next directory entry.
-readSync :: Dir -> Effect (Maybe (Dirent DirentNameString))
-readSync dir = toMaybe <$> runEffectFn1 readSyncImpl dir
+read :: Dir -> Effect (Maybe (Dirent DirentNameString))
+read dir = toMaybe <$> runEffectFn1 readSyncImpl dir
